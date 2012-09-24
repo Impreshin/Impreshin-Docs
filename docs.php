573e7ec5766d6b353b970b3e035c3fa56a3c94f5
William Stam <awstam@gmail.com>
2012-09-24 10:51:30 +0200

    Removed some un-necessary files

---
+++ docs.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Date: 2012/09/21
+ * Time: 12:35 PM
+ */
+
+
+$docs['ab']=array(
+	"form"=>array(
+		"add"=>array(
+			"heading"=>"Adding booking",
+			"description"=>"Tutorial on how to add a booking",
+			"swf"=>"ab#form-add.swf"
+		)
+	),
+	"bookings"=>array(
+		"settings"=>array(
+			"heading"=>"Change booking list settings",
+			"description"=>"Tutorial on how to change the lists settings",
+			"swf"=>"ab#form-add.swf"
+		)
+	),
+
+);
