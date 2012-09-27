082153598a3dea41c93382af63cecd49726d557c
William Stam <awstam@gmail.com>
2012-09-27 15:09:17 +0200

    docs

--- docs.php
+++ docs.php
@@ -5,20 +5,135 @@
  */
 
 
+//test_array($user);
+function perm(){
+	$args = func_get_args();
+	$user = F3::get("user");
+	$permission = $user['permissions'];
+
+	foreach($args as $arg){
+		if (isset($permission[$arg])){
+			$permission = $permission[$arg];
+		} else {
+			$permission = 0;
+			break;
+		}
+	}
+
+	return ($permission == '1') ? 1 : 0;
+}
 $docs['ab']=array(
 	"form"=>array(
-		"add"=>array(
-			"heading"=>"Adding booking",
-			"description"=>"Tutorial on how to add a booking",
-			"swf"=>"ab#form-add.swf"
+		"title"=>"Form",
+		"help"=>array(
+				"add"=>array(
+					"heading"    => "Adding booking",
+					"description"=> "Help on how to add a booking",
+					"file"=> "ab#form-add.html",
+					"tutorial"=>"",
+					"faq"=>"",
+					"p"=>perm('form','new')
+				),
+				"edit"=>array(
+					"heading"    => "Editing a booking",
+					"description"=> "Tutorial on how to add a booking",
+					"file"=> "ab#form-edit.html",
+					"tutorial"=> "",
+					"faq"     => "",
+					"p"=> perm('form', 'edit')
+				),
+				"delete"=>array(
+					"heading"    => "Deleting a booking",
+					"description"=> "Tutorial on how to add a booking",
+					"file"=> "ab#form-delete.html",
+					"tutorial"=> "",
+					"faq"     => "",
+					"p"=> perm('form', 'delete')
+				)
 		)
+
 	),
 	"bookings"=>array(
-		"settings"=>array(
-			"heading"=>"Change booking list settings",
-			"description"=>"Tutorial on how to change the lists settings",
-			"swf"=>"ab#form-add.swf"
+		"title"=>"Bookings",
+		"help"=>array(
+				"settings"=>array(
+					"heading"    => "Change settings",
+					"description"=> "Help on how to add a booking",
+					"file"=> "ab#form-add.html",
+					"tutorial"=>"",
+					"faq"=>"",
+					"p"=> 1
+				),
+				"highlight"=>array(
+					"heading"    => "highlight different records",
+					"description"=> "Tutorial on how to add a booking",
+					"file"=> "ab#form-edit.html",
+					"tutorial"=> "",
+					"p"=> 0
+				),
+				"filter"=>array(
+					"heading"    => "filter bookings",
+					"description"=> "Tutorial on how to add a booking",
+					"file"=> "ab#form-delete.html",
+					"tutorial"=> "",
+					"faq"     => "",
+					"p"=> 1
+				),
+				"tips"=>array(
+					"heading"    => "Bookings Tips",
+					"description"=> "Tutorial on how to add a booking",
+					"file"=> "ab#form-delete.html",
+					"tutorial"=> "",
+					"faq"     => "",
+					"p"=> 1
+				)
 		)
+
 	),
+
+	"admin"=>array(
+		"title"=>"Administration",
+		"help"=>array(
+			"sections"=>array(
+				"heading"    => "Administration Sections",
+				"description"=> "the difference between the sections",
+				"file"       => "",
+				"tutorial"   => "",
+				"faq"        => "",
+				"p"=> perm('administration', '_nav')
+			),
+
+			"users"=>array(
+				"title"=> "Users",
+				"help" => array(
+					"permissions"=>array(
+						"heading"    => "User Permissions",
+						"description"=> "Help on how to change permissions",
+						"file"       => "",
+						"tutorial"   => "",
+						"faq"        => "",
+						"p"=> perm('administration','system', 'users','page')
+					),
+					"add"=>array(
+						"heading"    => "add a user",
+						"description"=> "Help on how to add a user",
+						"file"       => "",
+						"tutorial"   => "",
+						"faq"        => "",
+						"p"=> perm('administration', 'system', 'users', 'page')
+					),
+					"delete"=>array(
+						"heading"    => "remove a user",
+						"description"=> "Help on how to remove a user",
+						"file"       => "",
+						"tutorial"   => "",
+						"faq"        => "",
+						"p"=> perm('administration', 'system', 'users', 'page')
+					),
+				)
+			)
+		)
+	)
+
 
 );
