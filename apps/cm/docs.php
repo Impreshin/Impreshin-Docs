72cb8256ad1be7535c5ecdfb61b2b72e3a144377
William Stam <awstam@gmail.com>
2014-09-12 12:37:08 +0200

    AdBooker + Added Records -> Marketer Records (can select an individual marketer and it shows their records)
    Contact Manager WIP

---
+++ apps/cm/docs.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Date: 2013/12/02
+ * Time: 11:12 AM
+ */
+$app = "cm";
+$docs[$app] = array(
+	"heading"    => $applications[$app]['name'],
+	"description"=> $applications[$app]['description'],
+	 "file"       => "index.html",
+	 "tutorial"   => "",
+	 "p"          => 1,
+	 "sub"=>array()
+		
+ )
+ 
+ ?>
+ 
